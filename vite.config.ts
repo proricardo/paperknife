fa08c15ba1ad84453586e9f0cda844efa68ec65c
potatameister <180633607+potatameister@users.noreply.github.com>
2026-01-29 20:18:41 +0700

    chore: add GitHub Pages deployme     workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PaperKnife/',
   server: {
-    host: true, // This allows you to view the app on your phone's browser
-    port: 5173
+    host: true
   }
 })
